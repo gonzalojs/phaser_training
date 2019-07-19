aa651b0b62af77989ae42f38b326d07867dba4cc
gonzalojs <contacto@ulterweb.com>
2019-07-19 16:03:08 -0400

    play button

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -9,8 +9,43 @@ class TitleScene extends Phaser.Scene {
   }
   create () {
     console.log('Title Scene!')
+
+    this.gameButton = this.add.sprite(100, 200, 'button').setInteractive()
+    this.gameButton.setScale(0.2)
+    this.centerButton(this.gameButton, 1)
+
+    this.gameText = this.add.text(0, 0, 'Play', {
+      fontSize: '32px',
+      fill: '#fff'
+    })
+    this.centerButtonText(this.gameText, this.gameButton)
+
+    this.gameButton.on('pointerdown', function (pointer) {
+      this.scene.start('Game')
+    }.bind(this))
+
+    this.input.on('pointerover', function (event, gameObject) {
+      gameObject[0].setTexture('button')
+    })
+
+
   }
   update () {
     
   }
+
+  centerButton (gameObject, offset = 0) {
+    Phaser.Display.Align.In.Center(
+      gameObject,
+      this.add.zone(config.width/2, config.height/2 - offset * 100, config.width, config.height)
+    )
+  }
+
+  centerButtonText (gameText, gameButton) {
+    Phaser.Display.Align.In.Center(
+      gameText,
+      gameButton
+    )
+  }
+
 }
