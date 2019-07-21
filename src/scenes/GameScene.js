0121676e7a01dc9060b0b18c892ce4d4dd6019b3
gonzalojs <contacto@ulterweb.com>
2019-07-20 22:57:28 -0400

    add 9 orange bottoms

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -10,12 +10,31 @@ class GameScene extends Phaser.Scene {
   create () {
     console.log('Game Scene!')
 
-    console.log(this.sys.canvas.width, this.sys.canvas.height)
+    const BACK = this.add.image(0, 0, 'back')
+    const TOP_LEFT = this.add.image(0, 0, 'orange')
+    const TOP_CENTER = this.add.image(0, 0, 'orange')
+    const TOP_RIGHT = this.add.image(0, 0, 'orange')
+    const MIDDLE_LEFT = this.add.image(0, 0, 'orange')
+    const MIDDLE_CENTER = this.add.image(0, 0, 'orange')
+    const MIDDLE_RIGHT = this.add.image(0, 0, 'orange')
+    const BOTTOM_LEFT = this.add.image(0, 0, 'orange')
+    const BOTTOM_CENTER = this.add.image(0, 0, 'orange')
+    const BOTTOM_RIGHT = this.add.image(0, 0, 'orange')
 
-    let back = this.add.image(0, 0, 'back')
-    back.width = this.sys.canvas.width / 10
-    back.height = this.sys.canvas.height
-    Phaser.Display.Align.In.Center(back, this.add.zone(400, 300, 800, 600))
+
+
+    Phaser.Display.Align.In.Center(BACK, this.add.zone(400, 300, 800, 600))
+
+    //center sprite in the picture
+    Phaser.Display.Align.In.TopLeft(TOP_LEFT, BACK)
+    Phaser.Display.Align.In.TopCenter(TOP_CENTER, BACK)
+    Phaser.Display.Align.In.TopRight(TOP_RIGHT, BACK)
+    Phaser.Display.Align.In.LeftCenter(MIDDLE_LEFT, BACK)
+    Phaser.Display.Align.In.Center(MIDDLE_CENTER, BACK)
+    Phaser.Display.Align.In.RightCenter(MIDDLE_RIGHT, BACK)
+    Phaser.Display.Align.In.BottomLeft(BOTTOM_LEFT, BACK)
+    Phaser.Display.Align.In.BottomCenter(BOTTOM_CENTER, BACK)
+    Phaser.Display.Align.In.BottomRight(BOTTOM_RIGHT, BACK)
   }
   update () {
     
