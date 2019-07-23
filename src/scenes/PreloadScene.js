933c6e7570681e4de6730a2ffb8cd3df1fd9d04d
gonzalojs <contacto@ulterweb.com>
2019-07-23 18:37:21 -0400

    sounds

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -84,8 +84,8 @@ class PreloadScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 48
     })
-    this.load.audio('pickup', 'src/sounds/item-pickup.ogg')
-    this.load.audio('bonus', 'src/sounds/bonus.wav')
+    this.load.audio('pickup', 'src/sounds/pickup.wav')
+    this.load.audio('bonus', 'src/sounds/item-pickup.ogg')
 /*     for (let i = 0; i < 50; i++) {
       this.load.image('logos'+i, 'src/sprites/logos.jpg')
     }
